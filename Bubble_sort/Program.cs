5ac5cf2349e4f917eb527d998a9252dc8793fea8
rezariswandhaa <emrezariswanda@gmail.com>
2022-10-06 19:19:02 +0700

    add void Bubble story array

--- Bubble_sort/Program.cs
+++ Bubble_sort/Program.cs
@@ -50,6 +50,23 @@
             }
             Console.WriteLine("");
         }
-        public void 
+        public void BubbleSortArray()
+        {
+            for (int i = 1; i < n; i++)// for n - 1 passes
+            {
+                //pada pass i, bandingkan n - i elemen pertama dengan elemen selanjutnya
+                for (int j = 0; j < n - i; j++)
+                {
+                    if (a[j] > a[j + 1]) //jika elemen tidak dalam urutan yang benar
+                    {
+                        //tukar elemen
+                        int temp;
+                        temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                    }
+                }
+            }
+        }
     }
 }
